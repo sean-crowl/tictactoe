4a931f0d0989491080b0fb8202455c31054e5d08
Sean Crowl <sean.crowl@interapthq.com>
2016-09-28 14:58:03 -0400

    Almost done!

--- tictactoe.js
+++ tictactoe.js
@@ -10,6 +10,7 @@ function gameRestartX() {
     $("#owins").hide();
     $("#xturn").show();
     $("#oturn").hide();
+    $("#board1, #board2, #board3, #board4, #board5, #board6, #board7, #board8, #board9").css("color", "cyan");
     createX();
 
 }
@@ -24,6 +25,7 @@ function gameRestartO() {
     $("#owins").hide();
     $("#xturn").hide();
     $("#oturn").show();
+    $("#board1, #board2, #board3, #board4, #board5, #board6, #board7, #board8, #board9").css("color", "cyan");
     createX();
 
 }
@@ -67,126 +69,127 @@ function victory() {
         $("#xwins").show();
         $("#xturn").hide();
         $("#oturn").hide();
+        $("#board1, #board2, #board3").css("color", "red");
     }
     if ($("#board4").text() == 'X' && $("#board5").text() == 'X' && $("#board6").text() == 'X') {
         $(".tic").off('click'); 
-        $("#boardfull").hide();
         $("#restartx").show();
         $("#xwins").show();
         $("#xturn").hide();
         $("#oturn").hide();
+        $("#board4, #board5, #board6").css("color", "red");
     }
     if ($("#board7").text() == 'X' && $("#board8").text() == 'X' && $("#board9").text() == 'X') {
         $(".tic").off('click'); 
-        $("#boardfull").hide();
         $("#restartx").show();
         $("#xwins").show();
         $("#xturn").hide();
         $("#oturn").hide();
+        $("#board7, #board8, #board9").css("color", "red");
     }
     if ($("#board1").text() == 'X' && $("#board4").text() == 'X' && $("#board7").text() == 'X') {
         $(".tic").off('click'); 
-        $("#boardfull").hide();
         $("#restartx").show();
         $("#xwins").show();
         $("#xturn").hide();
         $("#oturn").hide();
+        $("#board1, #board4, #board7").css("color", "red");
     }
     if ($("#board2").text() == 'X' && $("#board5").text() == 'X' && $("#board8").text() == 'X') {
         $(".tic").off('click'); 
-        $("#boardfull").hide();
         $("#restartx").show();
         $("#xwins").show();
         $("#xturn").hide();
         $("#oturn").hide();
+        $("#board2, #board5, #board8").css("color", "red");
     }
     if ($("#board3").text() == 'X' && $("#board6").text() == 'X' && $("#board9").text() == 'X') {
         $(".tic").off('click'); 
-        $("#boardfull").hide();
         $("#restartx").show();
         $("#xwins").show();
         $("#xturn").hide();
         $("#oturn").hide();
+        $("#board3, #board6, #board9").css("color", "red");
     }
     if ($("#board1").text() == 'X' && $("#board5").text() == 'X' && $("#board9").text() == 'X') {
         $(".tic").off('click'); 
-        $("#boardfull").hide();
         $("#restartx").show();
         $("#xwins").show();
         $("#xturn").hide();
         $("#oturn").hide();
+        $("#board1, #board5, #board9").css("color", "red");
     }
     if ($("#board3").text() == 'X' && $("#board5").text() == 'X' && $("#board7").text() == 'X') {
         $(".tic").off('click'); 
-        $("#boardfull").hide();
         $("#restartx").show();
         $("#xwins").show();
         $("#xturn").hide();
         $("#oturn").hide();
+        $("#board3, #board5, #board7").css("color", "red");
     }
     if ($("#board1").text() == 'O' && $("#board2").text() == 'O' && $("#board3").text() == 'O') {
         $(".tic").off('click'); 
-        $("#boardfull").hide();
         $("#restarto").show();
         $("#owins").show();
         $("#xturn").hide();
         $("#oturn").hide();
+        $("#board1, #board2, #board3").css("color", "red");
     }
     if ($("#board4").text() == 'O' && $("#board5").text() == 'O' && $("#board6").text() == 'O') {
         $(".tic").off('click'); 
-        $("#boardfull").hide();
         $("#restarto").show();
         $("#owins").show();
         $("#xturn").hide();
         $("#oturn").hide();
+        $("#board4, #board5, #board6").css("color", "red");
     }
     if ($("#board7").text() == 'O' && $("#board8").text() == 'O' && $("#board9").text() == 'O') {
         $(".tic").off('click'); 
-        $("#boardfull").hide();
         $("#restarto").show();
         $("#owins").show();
         $("#xturn").hide();
         $("#oturn").hide();
+        $("#board7, #board8, #board9").css("color", "red");
     }
     if ($("#board1").text() == 'O' && $("#board4").text() == 'O' && $("#board7").text() == 'O') {
         $(".tic").off('click'); 
-        $("#boardfull").hide();
         $("#restarto").show();
         $("#owins").show();
         $("#xturn").hide();
         $("#oturn").hide();
+        $("#board1, #board4, #board7").css("color", "red");
     }
     if ($("#board2").text() == 'O' && $("#board5").text() == 'O' && $("#board8").text() == 'O') {
         $(".tic").off('click'); 
-        $("#boardfull").hide();
         $("#restarto").show();
         $("#owins").show();
         $("#xturn").hide();
         $("#oturn").hide();
+        $("#board2, #board5, #board8").css("color", "red");
     }
     if ($("#board3").text() == 'O' && $("#board6").text() == 'O' && $("#board9").text() == 'O') {
         $(".tic").off('click'); 
-        $("#boardfull").hide();
         $("#restarto").show();
         $("#owins").show();
         $("#xturn").hide();
         $("#oturn").hide();
+        $("#board3, #board6, #board9").css("color", "red");
     }
     if ($("#board1").text() == 'O' && $("#board5").text() == 'O' && $("#board9").text() == 'O') {
         $(".tic").off('click'); 
-        $("#boardfull").hide();
         $("#restarto").show();
         $("#owins").show();
         $("#xturn").hide();
         $("#oturn").hide();
+        $("#board1, #board5, #board9").css("color", "red");
     }
     if ($("#board3").text() == 'O' && $("#board5").text() == 'O' && $("#board7").text() == 'O') {
         $(".tic").off('click'); 
-        $("#boardfull").hide();
         $("#restarto").show();
         $("#owins").show();
         $("#xturn").hide();
         $("#oturn").hide();
+        $("#board3, #board5, #board7").css("color", "red");
     }
 }
 
